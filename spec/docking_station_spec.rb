09e912230c4b1319b0f26c5d56726bf43c28ec9c
Jack Dewinter <jackdewinter1@gmail.com>
2018-03-20 17:09:06 +0000

    Combined tests.

--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -3,12 +3,8 @@ require "./bike"
 
 describe DockingStation do
   it {respond_to(:release_bike)}
-    #bike = DockingStation.new.release_bike
-    #expect(bike.working?).to eq(true)
-end
-
-describe Bike do
-   it "expects bike to be working" do
-     expect(Bike.new.working?).to eq(true)
-   end
+  it "Bike is working" do
+    bike = DockingStation.new.release_bike
+    expect(bike.working?).to eq(true)
+  end
 end

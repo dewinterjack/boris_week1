ccc7df04cc3179a99087f07f6b31cae8c9860309
Jack Dewinter <jackdewinter1@gmail.com>
2018-03-21 15:48:50 +0000

    Changed dockingstation.new to subject.

--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -2,7 +2,7 @@ require "./docking_station"
 
 describe DockingStation do
   it "releases a bike that works." do
-    bike = DockingStation.new.release_bike
+    bike = subject.release_bike
     expect(bike.working?).to eq(true)
   end
   it {is_expected.to respond_to(:dock_bike)}

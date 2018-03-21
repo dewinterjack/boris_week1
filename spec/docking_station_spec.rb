de5a3fa12a66aa5bc2ed6d5230e3ed1e891f4512
Jack Dewinter <jackdewinter1@gmail.com>
2018-03-21 16:23:08 +0000

    Test expects 1 argument for dock_bike but fails.

--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -1,9 +1,12 @@
 require "./docking_station"
 
+
 describe DockingStation do
+
   it "releases a bike that works." do
     bike = subject.release_bike
     expect(bike.working?).to eq(true)
   end
-  it {is_expected.to respond_to(:dock_bike)}
+  
+  it {is_expected.to respond_to(:dock_bike).with(1).argument}
 end
